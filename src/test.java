422af1e64de83dc7e2f397d5aec69fdeecde354d
adem akhoy <akhoy.adem@gmail.com>
2021-02-25 17:59:16 -0600

    sepete ekle kismi ekledni

--- src/test.java
+++ src/test.java
@@ -1,5 +1,13 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("1.islem");
+
+
+    }
+
+    public static void yeniMethod()
+    {
+        System.out.println("yeni methodum hazir");
+        System.out.println("sepete ekle kisimlari buralar");
     }
 }
